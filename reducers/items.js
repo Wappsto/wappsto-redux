4c72c457ec710a9be1e2fa96fb934e11cf620d45
Sami <sami@seluxit.com>
2019-06-06 11:31:59 +0200

    handle item data as a function

--- reducers/items.js
+++ reducers/items.js
@@ -9,13 +9,21 @@ const initialState = {};
 export default function reducer(state = initialState, action){
   switch(action.type){
     case ADD_ITEM:
+      let data;
+      if(action.data.constructor === Function){
+        data = action.data(state[action.name]);
+      } else {
+        data = action.data;
+      }
       return Object.assign({}, state, {
-        [action.name]: action.data
+        [action.name]: data
       });
     case REMOVE_ITEM:
       state = Object.assign({}, state);
       delete state[action.name];
       return state;
+    default:
+      break;
   }
   return state;
 }
