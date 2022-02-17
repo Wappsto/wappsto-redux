d77f8af66436668b084b8729b6c957cb21f8aa1f
Andreas Bomholtz <andreas@seluxit.com>
2022-02-17 10:27:20 +0100

    Added tests for events

---
+++ __tests__/events.test.js
@@ -0,0 +1,20 @@
+import { onLogout, trigger } from '../events';
+
+describe('events', () => {
+    it('trigger an logout event', () => {
+        let fun = jest.fn();
+        onLogout(fun);
+        trigger('logout', 'test')
+
+        expect(fun).toHaveBeenCalledTimes(1)
+        expect(fun).toHaveBeenCalledWith('test')
+    });
+
+    it('will not trigger an logout event with an invalid event', () => {
+        let fun = jest.fn();
+        onLogout(fun);
+        trigger('login', 'test')
+
+        expect(fun).toHaveBeenCalledTimes(0)
+    });
+});
